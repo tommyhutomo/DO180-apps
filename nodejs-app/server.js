da85a02785fcefc61eee90be7ef396a1f4961998
tommyhutomo <tommy.hutomo30@gmail.com>
2020-07-11 14:33:43 +0800

    Update server.js

--- nodejs-app/server.js
+++ nodejs-app/server.js
@@ -4,7 +4,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World from pod: ' + process.environment.HOSTNAME + '\n')
+  res.send('Hello World from pod: ' + process.environment.env.HOSTNAME + '\n')
 });
 
 app.listen(8080, function () {
